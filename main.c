f9626f302d8f26ffa8ba909d5e970e50b2648326
Luis Paulo Nery <l.pns@hotmail.com>
2016-09-03 18:44:53 -0300

    programa funcional

--- main.c
+++ main.c
@@ -12,20 +12,41 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-/* run this program using the console pauser or add your own getch, system("pause") or input loop */
-// cria variavel inteira
-int numero = 2;
-// defini a posicao de memoria que o pornteiro ira armazenar
-int *ptr_numero = &numero;
-//inicia a funçao
-int main(int argc, char *argv[]) 
+int main ()
 {
-	// mostra uma mensagem
-	printf ("Digite um numero: ");
-	// espera o usuario entrar com um valor
-	scanf ("%i", ptr_numero);
-	// mostra o numero digitado pelo usuario a partir do ponteiro
-	printf ("O numero digitado foi: %i %i %i!\n", &ptr_numero, ptr_numero, *ptr_numero);
-	//retora zero
+	// criação e inicialização de variaveis 
+	int variavel_1 = 1, variavel_2 = 2;
+	char controle = 'n', descarga;
+	
+	//criação de ponteiros
+	int *ponteiro_1, *ponteiro_2;
+	
+	//enquanto
+	while (1)
+	{
+		//inicializaoção dos ponteiros
+		ponteiro_1 = &variavel_1; // ponteiro_1 recebe endereço de variavel_1
+		ponteiro_2 = &variavel_2; // ponteiro_2 recebe endereço de variavel_2
+		
+		//exibe o valor em cada ponteiro a partir do endereço contido neles
+		printf ("\nVarivel_1 = %i", *ponteiro_1);
+		printf ("\nVarivel_2 = %i", *ponteiro_2);
+		
+		//exibe a solicitação de controle
+		printf ("\n\nDeseja finalizar o programa? (s/n)");
+		scanf ("%c", &controle);
+		//caso atenda o caso, irá finalizar o loop
+		if (controle == 's') break;
+		
+		//exibe a solicitação de valor
+		printf ("Digite um valor para variavel_1: ");
+		//efetua a leitura de e tranfere o valor para variavel_1
+		scanf ("%i", ponteiro_1);
+		//exibe a solicitação de valor
+		printf ("Digite um valor para variavel_2: ");
+		//efetua a leitura de e tranfere o valor para variavel_2
+		scanf ("%i%c", ponteiro_2, &descarga);		
+	}
+	//retorna 0
 	return 0;
 }
